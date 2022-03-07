9f0443c1c1de6bffeb5b0119feb96602fb073053
hujing39 <hujing39@jd.com>
2022-03-07 13:53:07 +0800

    add wjob-rpc

---
+++ wjob-rpc/src/main/java/com/kve/rpc/ServiceCenter.java
@@ -0,0 +1,123 @@
+package com.kve.rpc;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Method;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author: hujing39
+ * @date: 2022-03-07
+ */
+public class ServiceCenter implements RpcServer {
+    private static ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private static final HashMap<String, Class> serviceRegistry = new HashMap<String, Class>();
+    private static boolean isRunning = false;
+    private static int port;
+    private static String host;
+
+    public ServiceCenter(String host, int port) {
+        ServiceCenter.port = port;
+        ServiceCenter.host = host;
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+        executor.shutdown();
+    }
+
+    @Override
+    public void start() throws IOException {
+        ServerSocket server = new ServerSocket();
+        server.bind(new InetSocketAddress(host, port));
+        System.out.println("start server");
+        try {
+            while (true) {
+                //监听客户端的TCP连接 收到TCP连接后封装成Task 由线程池执行
+                executor.execute(new ServiceTask(server.accept()));
+            }
+        } finally {
+            server.close();
+        }
+    }
+
+    @Override
+    public void Register(Class serverInterface, Class impl) {
+        serviceRegistry.put(serverInterface.getName(), impl);
+    }
+
+    @Override
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    @Override
+    public int getPort() {
+        return port;
+    }
+
+    private static class ServiceTask implements Runnable {
+        Socket client = null;
+
+        public ServiceTask(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            ObjectInputStream input = null;
+            ObjectOutputStream output = null;
+
+            try {
+                //将客户端发送的反序列化成对象 反射调用服务实现者 获取执行结果
+                input = new ObjectInputStream(client.getInputStream());
+                String serviceName = input.readUTF();
+                String methodName = input.readUTF();
+                Class<?>[] parameterTypes = (Class<?>[]) input.readObject();
+                Object[] arguments = (Object[]) input.readObject();
+                Class serviceClass = serviceRegistry.get(serviceName);
+                if (serviceClass == null) {
+                    throw new ClassNotFoundException(serviceName + "not found");
+                }
+                Method method = serviceClass.getMethod(methodName, parameterTypes);
+                Object result = method.invoke(serviceClass.newInstance(), arguments);
+
+                //将执行结果反序列化 通过socket发送给客户端
+                output = new ObjectOutputStream(client.getOutputStream());
+                output.writeObject(result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (output != null) {
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (input != null) {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (client != null) {
+                    try {
+                        client.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
